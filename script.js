f0db8781f1223754eff33a490ec41cb42facbd3d
junwenhu777 <hujunwen119@gmail.com>
2023-06-02 15:26:55 +0800

    modify the camera

--- script.js
+++ script.js
@@ -2,9 +2,25 @@
 var video = document.getElementById('video');
 
 // Check if the browser supports media devices and getUserMedia
-if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+if (navigator.mediaDevices && navigator.mediaDevices.enumerateDevices) {
   // Request permission to access the camera
-  navigator.mediaDevices.getUserMedia({ video: true })
+  navigator.mediaDevices.enumerateDevices()
+    .then(function(devices) {
+      // Find the video input device
+      var videoDevices = devices.filter(function(device) {
+        return device.kind === 'videoinput';
+      });
+
+      if (videoDevices.length > 0) {
+        // Use the first video input device found
+        var constraints = { video: { deviceId: videoDevices[0].deviceId } };
+
+        // Request permission to access the camera with constraints
+        return navigator.mediaDevices.getUserMedia(constraints);
+      } else {
+        throw new Error('No video input devices found.');
+      }
+    })
     .then(function(stream) {
       // Set the video source to the user's camera stream
       video.srcObject = stream;
