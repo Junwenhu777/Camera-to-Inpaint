45de1b66ffa317d3134580f597798e8722945a31
junwenhu777 <hujunwen119@gmail.com>
2023-06-02 16:44:10 +0800

    modify

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const video = document.getElementById('video');
     const canvas = document.getElementById('canvas');
     const context = canvas.getContext('2d');
+    let videoWidth, videoHeight;
   
     // Check if the browser supports getUserMedia
     const hasUserMedia = !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
@@ -10,9 +10,13 @@ document.addEventListener('DOMContentLoaded', () => {
     if (hasUserMedia) {
       navigator.mediaDevices.getUserMedia({ video: true })
         .then((stream) => {
+          const video = document.createElement('video');
           video.srcObject = stream;
           video.onloadedmetadata = () => {
             video.play();
+            videoWidth = video.videoWidth;
+            videoHeight = video.videoHeight;
+            adjustCanvasSize();
             requestAnimationFrame(captureFrame);
           };
         })
@@ -23,12 +27,30 @@ document.addEventListener('DOMContentLoaded', () => {
       console.log('getUserMedia is not supported in this browser.');
     }
   
+    // Adjust canvas size to match video dimensions
+    function adjustCanvasSize() {
+      const windowWidth = window.innerWidth;
+      const windowHeight = window.innerHeight;
+      const windowAspectRatio = windowWidth / windowHeight;
+      const videoAspectRatio = videoWidth / videoHeight;
+  
+      if (windowAspectRatio < videoAspectRatio) {
+        canvas.width = videoAspectRatio * windowHeight;
+        canvas.height = windowHeight;
+      } else {
+        canvas.width = windowWidth;
+        canvas.height = windowWidth / videoAspectRatio;
+      }
+    }
+  
     // Capture video frame and draw on canvas
     function captureFrame() {
-      canvas.width = video.videoWidth;
-      canvas.height = video.videoHeight;
       context.drawImage(video, 0, 0, canvas.width, canvas.height);
       requestAnimationFrame(captureFrame);
     }
+  
+    // Adjust canvas size when the window is resized
+    window.addEventListener('resize', adjustCanvasSize);
   });
+  
   
