2adc6b6e05b28bf83c835dd150f35b536fc0b0f8
junwenhu777 <hujunwen119@gmail.com>
2023-06-02 14:10:42 +0800

    create video view

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('Hello, World!');
+// Get video and canvas elements
+const video = document.getElementById('camera');
+const canvas = document.getElementById('canvas');
+const captureBtn = document.getElementById('captureBtn');
+
+// Get user media
+navigator.mediaDevices.getUserMedia({ video: true })
+  .then(stream => {
+    video.srcObject = stream;
+  })
+  .catch(error => {
+    console.error('Error accessing camera:', error);
+  });
+
+// Capture button click event
+captureBtn.addEventListener('click', () => {
+  // Draw the video frame on the canvas
+  canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+});
+
