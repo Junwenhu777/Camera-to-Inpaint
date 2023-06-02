59d04eab31eb966a7b0003df0a63e57b5ea56a66
junwenhu777 <hujunwen119@gmail.com>
2023-06-02 16:38:05 +0800

    modify

--- script.js
+++ script.js
@@ -1,43 +1,34 @@
-// Access the video element
-var video = document.getElementById('video');
-
-// Check if the browser supports media devices and getUserMedia
-if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-  // Request permission to access the camera
-  navigator.mediaDevices.getUserMedia({ video: true })
-    .then(function(stream) {
-      // Set the video source to the user's camera stream
-      video.srcObject = stream;
-    })
-    .catch(function(error) {
-      console.error('Error accessing camera:', error);
-    });
-} else if (navigator.mediaDevices && navigator.mediaDevices.enumerateDevices) {
-  // Request permission to access the camera using enumerateDevices
-  navigator.mediaDevices.enumerateDevices()
-    .then(function(devices) {
-      // Find the video input device
-      var videoDevices = devices.filter(function(device) {
-        return device.kind === 'videoinput';
-      });
-
-      if (videoDevices.length > 0) {
-        // Use the first video input device found
-        var constraints = { video: { deviceId: videoDevices[0].deviceId } };
-
-        // Request permission to access the camera with constraints
-        return navigator.mediaDevices.getUserMedia(constraints);
-      } else {
-        throw new Error('No video input devices found.');
-      }
-    })
-    .then(function(stream) {
-      // Set the video source to the user's camera stream
-      video.srcObject = stream;
-    })
-    .catch(function(error) {
-      console.error('Error accessing camera:', error);
-    });
-} else {
-  console.error('getUserMedia is not supported by this browser.');
-}
+document.addEventListener('DOMContentLoaded', () => {
+    const video = document.getElementById('video');
+    const canvas = document.getElementById('canvas');
+    const context = canvas.getContext('2d');
+  
+    // Check if the browser supports getUserMedia
+    const hasUserMedia = !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+  
+    // Initialize camera stream
+    if (hasUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true })
+        .then((stream) => {
+          video.srcObject = stream;
+          video.onloadedmetadata = () => {
+            video.play();
+            requestAnimationFrame(captureFrame);
+          };
+        })
+        .catch((error) => {
+          console.log('Error accessing camera:', error);
+        });
+    } else {
+      console.log('getUserMedia is not supported in this browser.');
+    }
+  
+    // Capture video frame and draw on canvas
+    function captureFrame() {
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      context.drawImage(video, 0, 0, canvas.width, canvas.height);
+      requestAnimationFrame(captureFrame);
+    }
+  });
+  
