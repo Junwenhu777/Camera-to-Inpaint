84549e2782d2cfd819ddaa9f84e8acba4325e662
junwenhu777 <hujunwen119@gmail.com>
2023-06-02 14:49:50 +0800

    modify interface

--- script.js
+++ script.js
@@ -1,20 +1,43 @@
-// Get video and canvas elements
-const video = document.getElementById('camera');
-const canvas = document.getElementById('canvas');
-const captureBtn = document.getElementById('captureBtn');
+// Access the canvas element and get its 2D rendering context
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
 
-// Get user media
-navigator.mediaDevices.getUserMedia({ video: true })
-  .then(stream => {
-    video.srcObject = stream;
-  })
-  .catch(error => {
-    console.error('Error accessing camera:', error);
-  });
+// Set the canvas size to match the window size
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-// Capture button click event
-captureBtn.addEventListener('click', () => {
-  // Draw the video frame on the canvas
-  canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+// Track the coordinates of the circle being drawn
+var startX, startY;
+var isDrawing = false;
+
+// Get the loading element
+var loadingElement = document.querySelector('.loading');
+
+// Event listener for touch start event
+canvas.addEventListener('touchstart', function(event) {
+  startX = event.touches[0].clientX;
+  startY = event.touches[0].clientY;
+  isDrawing = true;
 });
 
+// Event listener for touch move event
+canvas.addEventListener('touchmove', function(event) {
+  event.preventDefault();
+  if (!isDrawing) return;
+
+  var endX = event.touches[0].clientX;
+  var endY = event.touches[0].clientY;
+  var radius = Math.sqrt(Math.pow(endX - startX, 2) + Math.pow(endY - startY, 2));
+
+  // Clear the canvas before drawing the circle
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Draw the circle
+  ctx.beginPath();
+  ctx.arc(startX, startY, radius, 0, 2 * Math.PI);
+  ctx.strokeStyle = 'rgba(255, 255, 255, 0.8)';
+  ctx.lineWidth = 5;
+  ctx.stroke();
+});
+
+// Event
