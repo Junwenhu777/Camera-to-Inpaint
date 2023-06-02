a3c26c131f3601cf6bb1e86c90766824848d9a0a
junwenhu777 <hujunwen119@gmail.com>
2023-06-02 15:02:11 +0800

    modify

--- script.js
+++ script.js
@@ -1,43 +1,17 @@
-// Access the canvas element and get its 2D rendering context
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
+// Access the video element
+var video = document.getElementById('video');
 
-// Set the canvas size to match the window size
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-// Track the coordinates of the circle being drawn
-var startX, startY;
-var isDrawing = false;
-
-// Get the loading element
-var loadingElement = document.querySelector('.loading');
-
-// Event listener for touch start event
-canvas.addEventListener('touchstart', function(event) {
-  startX = event.touches[0].clientX;
-  startY = event.touches[0].clientY;
-  isDrawing = true;
-});
-
-// Event listener for touch move event
-canvas.addEventListener('touchmove', function(event) {
-  event.preventDefault();
-  if (!isDrawing) return;
-
-  var endX = event.touches[0].clientX;
-  var endY = event.touches[0].clientY;
-  var radius = Math.sqrt(Math.pow(endX - startX, 2) + Math.pow(endY - startY, 2));
-
-  // Clear the canvas before drawing the circle
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  // Draw the circle
-  ctx.beginPath();
-  ctx.arc(startX, startY, radius, 0, 2 * Math.PI);
-  ctx.strokeStyle = 'rgba(255, 255, 255, 0.8)';
-  ctx.lineWidth = 5;
-  ctx.stroke();
-});
-
-// Event
+// Check if the browser supports media devices and getUserMedia
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+  // Request permission to access the camera
+  navigator.mediaDevices.getUserMedia({ video: true })
+    .then(function(stream) {
+      // Set the video source to the user's camera stream
+      video.srcObject = stream;
+    })
+    .catch(function(error) {
+      console.error('Error accessing camera:', error);
+    });
+} else {
+  console.error('getUserMedia is not supported by this browser.');
+}
